32aae59d5d95564c36cb57f0efb7edbd7c7534d6
xljnc <jsj0804wt@126.com>
2019-01-01 22:20:22 +0800

    EchoServer by Xljnc 2019-Jan-01

--- src/main/java/com/wt/test/netty/echo/server/EchoServer.java
+++ src/main/java/com/wt/test/netty/echo/server/EchoServer.java
@@ -1,16 +1,26 @@
 package com.wt.test.netty.echo.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+
 /**
- * @author WuTian
- * @date 2018-12-04 15:07
+ * @author Xljnc
+ * @date 2019/1/1 22:10
  * @description
  */
+@Slf4j
 public class EchoServer {
-    public static void main(String[] args) {
-
-    }
 
     public void start() {
-            
+        int port = 8888;
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        EchoServerHandler echoServerHandler = new EchoServerHandler();
+        ServerBootstrap server = new ServerBootstrap();
+        server.group(eventLoopGroup).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port));
     }
 }
---
+++ src/main/java/com/wt/test/netty/echo/server/EchoServerHandler.java
@@ -0,0 +1,39 @@
+package com.wt.test.netty.echo.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author WuTian
+ * @date 2018-12-04 15:07
+ * @description
+ */
+@Slf4j
+@Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received:" + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        log.error("error occurs.", cause);
+        ctx.close();
+    }
+
+}
